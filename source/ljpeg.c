7f51339f75f077b3108ecc62bcc9b811caa9078b
sage-etcher <sage.codes@email.com>
2023-11-22 14:11:04 -0500

    did some reordering of the code and comments for handling events

--- source/ljpeg.c
+++ source/ljpeg.c
@@ -1,7 +1,7 @@
 /*
    source/ljpeg.c
    LJPEG main entry point source file
- 
+
    Copyright 2023 Sage I. Hendricks
 
    Licensed under the Apache License, Version 2.0 (the "License");
@@ -14,7 +14,7 @@
    distributed under the License is distributed on an "AS IS" BASIS,
    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
    See the License for the specific language governing permissions and
-   limitations under the License. 
+   limitations under the License.
 
 */
 
@@ -75,17 +75,17 @@ main (int argc, char *argv[])
     if (exit_code != EXIT_SUCCESS)
     {
         goto main_exit_3;
-    } 
+    }
 
     /* set the window size and display the window */
     SDL_SetWindowSize (g_win, (int)g_img.source.w, (int)g_img.source.h);
-    SDL_ShowWindow (g_win);    
+    SDL_ShowWindow (g_win);
 
     /* set the background color for transparent images */
     /* 255 255 255 == White */
     /*   0   0   0 == Black */
     SDL_SetRenderDrawColor (g_rend, 255, 255, 255, SDL_ALPHA_OPAQUE);
-    
+
     /* initial draw */
     SDL_RenderClear (g_rend);
     graphics_render (g_rend, &g_img);
@@ -115,16 +115,16 @@ main (int argc, char *argv[])
             }
         }
 
-        /* display the image */ 
+        /* display the image */
         SDL_RenderClear (g_rend);
         graphics_render (g_rend, &g_img);
         SDL_RenderPresent (g_rend);
-        
-    } 
+
+    }
 
 
     /* exit routines */
-main_exit_4: 
+main_exit_4:
     SDL_RWclose (g_img.rwop);
     SDL_DestroyTexture (g_img.texture);
 main_exit_3:
@@ -158,75 +158,61 @@ static void
 key_event (SDL_Event *evt)
 {
     SDL_Event e = *evt;
-    
-    /* ESC */
+
     if (e.key.keysym.sym == SDLK_ESCAPE)
     {
+        /* Escape */
         /* quit */
         g_runtime_bool = false;
     }
-    /* CTRL+1 or ALT+1 */
     else if ((e.key.keysym.sym == '1') && ((e.key.keysym.mod & (KMOD_CTRL | KMOD_ALT)) != 0))
     {
+        /* Ctrl 1 or Alt 1 */
         /* scale to half size */
         g_img.scale = 0.5;
     }
-    /* CTRL+2 or ALT+2 */
-    else if ((e.key.keysym.sym == '2') && ((e.key.keysym.mod & (KMOD_CTRL | KMOD_ALT)) != 0))
+    else if (((e.key.keysym.sym == '2') && ((e.key.keysym.mod & (KMOD_CTRL | KMOD_ALT)) != 0)) ||
+             ((e.key.keysym.sym == '0') && ((e.key.keysym.mod & KMOD_CTRL) != 0) && ((e.key.keysym.mod & KMOD_ALT) != 0)))
     {
+        /* Ctrl 2 or Alt 2 or Ctrl Alt 0 */
         /* scale to original size */
         g_img.scale = 1.0;
     }
-    /* CTRL+3 or ALT+3 */
     else if ((e.key.keysym.sym == '3') && ((e.key.keysym.mod & (KMOD_CTRL | KMOD_ALT)) != 0))
     {
+        /* Ctrl 3 or Alt 3 */
         /* scale to 2x size */
         g_img.scale = 2.0;
     }
-    /* CTRL+ALT+0 */
-    else if ((e.key.keysym.sym == '0') && ((e.key.keysym.mod & KMOD_CTRL) != 0) && ((e.key.keysym.mod & KMOD_ALT) != 0))
-    {
-        /* scale to original size */
-        g_img.scale = 1.0;
-    }
-    /* CTRL+'=' */
     else if ((e.key.keysym.sym == '=') && ((e.key.keysym.mod & KMOD_CTRL) != 0))
     {
+        /* Ctrl = */
         /* scale the image up */
         g_img.scale *= 2;
     }
-    /* CTRL+'-' */
     else if ((e.key.keysym.sym == '-') && ((e.key.keysym.mod & KMOD_CTRL) != 0))
     {
+        /* Ctrl - */
         /* scale the image down */
         g_img.scale /= 2;
     }
-    /* SHIFT+'r' */
-    else if ((e.key.keysym.sym == 'r') && ((e.key.keysym.mod & KMOD_SHIFT) != 0))
+    else if (((e.key.keysym.sym == 'r') && ((e.key.keysym.mod & KMOD_SHIFT) != 0)) ||
+              (e.key.keysym.sym == SDLK_LEFT))
     {
+        /* Shift r or Left Arrow */
         /* rotate image counter-clockwise by 90 degrees */
         graphics_texture_rotate (&g_img, COUNTERCLOCKWISE);
     }
-    /* 'r' */
-    else if (e.key.keysym.sym == 'r')
+    else if ((e.key.keysym.sym == 'r') ||
+             (e.key.keysym.sym == SDLK_RIGHT))
     {
+        /* r or Right Arrow */
         /* rotate image clockwise by 90 degrees */
         graphics_texture_rotate (&g_img, CLOCKWISE);
     }
-    else if (e.key.keysym.sym == SDLK_RIGHT)
-    {
-        /* rotate image clockwise by 90 degrees */
-        graphics_texture_rotate (&g_img, CLOCKWISE);
-    }
-    /* Left Arrow */
-    else if (e.key.keysym.sym == SDLK_LEFT)
-    {
-        /* rotate image counter-clockwise by 90 degrees */
-        graphics_texture_rotate (&g_img, COUNTERCLOCKWISE);
-    }
-    /* 'a' */
     else if (e.key.keysym.sym == 'a')
     {
+        /* a */
         /* reset image to original scale and rotation */
         g_img.scale = 1.0;
         g_img.rotation = 0.0;
@@ -236,24 +222,24 @@ key_event (SDL_Event *evt)
 
 static void
 mouse_btn_event (SDL_Event *evt)
-{ 
+{
     SDL_Event e = *evt;
 
-    /* Single Right Click */
     if (e.button.button == SDL_BUTTON_RIGHT)
-    {   
+    {
+        /* Right Click */
         /* quit */
         g_runtime_bool = false;
     }
-    /* Double Left Click */
     else if ((e.button.button == SDL_BUTTON_LEFT) && (e.button.clicks == 2))
     {
+        /* Left Click (double) */
         /* reset scale to default */
         g_img.scale = 1.0;
     }
-    /* Single Left Click */
     else if (e.button.button == SDL_BUTTON_LEFT)
     {
+        /* Left Click (hold) */
         /* move window */
         graphics_manual_move_window ();
     }
@@ -262,18 +248,18 @@ mouse_btn_event (SDL_Event *evt)
 
 static void
 mouse_wheel_event (SDL_Event *evt)
-{ 
+{
     SDL_Event e = *evt;
-    
-    /* Scroll Wheel Away */
+
     if (e.wheel.y >= 1)
     {
+        /* Scroll Wheel forward */
         /* increase scale by 10% */
         g_img.scale *= 1.10;
     }
-    /* Scroll Wheel Towards */
     else if (e.wheel.y <= -1)
     {
+        /* Scroll Wheel backward */
         /* decrease scale by 10% */
         g_img.scale /= 1.10;
     }
